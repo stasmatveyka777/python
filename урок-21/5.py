4cc758fce7213bbb5fb9de81916f0ca713225354
stasmatveyka777 <66476012+stasmatveyka777@users.noreply.github.com>
2021-12-24 22:58:31 +0200

    Delete урок-21 directory

--- урок-21/5.py
+++
@@ -1,7 +0,0 @@
-list1=["user_0","user_1","user_2"]
-list2=[]
-while list1:
-    i=list1.pop()
-    print(i)
-    list2.append(i)
-print(list2)
--- урок-21/6.py
+++
@@ -1,9 +0,0 @@
-julia=['sushi','pizza','cake']
-teacher=julia[:]
-print(julia)
-print(teacher)
-a=str(input("a="))
-teacher.append(a)
-print(teacher)
-for i in teacher:
-    print(i)
