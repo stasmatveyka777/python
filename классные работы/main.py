9c1b63658852e1983285fac43f762a45336c514b
stasmatveyka777 <66476012+stasmatveyka777@users.noreply.github.com>
2022-02-22 17:54:40 +0200

    Add files via upload

---
+++ классные работы/main.py
@@ -0,0 +1,91 @@
+#1 zadanie
+def a1(name):
+    print(f"Hello {name}\n How are you?")
+
+#name= str(input("Write name \n"))
+#ima(name)
+
+#2 zadanie
+def a2(a, b):
+    print((a + " " ) * b)
+
+#a=str(input("Write str: "))
+#b=int(input("Write int: "))
+#a2(a,b)
+
+#6 zadanie
+#a=int(input("a="))
+#b=int(input("b="))
+#if a>b:
+ #   print("Int", a, "biger")
+#elif a==b:
+    #print("equal")
+#else:
+  #  print("Int", b, "biger")
+#7 zdanie
+def main(a,b,c):
+    if a==b==c:
+        print("equal")
+    else:
+        print(max(list))
+a=int(input("a="))
+b=int(input("b= "))
+c=int(input("c= "))
+list=[a,b]
+
+
+
+
+
+#a=int(input("a= "))
+#b=int(input("b= "))
+#c=int(input("c= "))
+
+
+
+#9 zadanie
+#def fuction(s1,s2):
+#    print(s1+""+s2)
+#s1=str(input("s1= "))
+#s2=str(input("s2= "))
+#fuction(s1,s2)
+
+#8 zadanie
+#def fuction8(a,b,c):
+#    if a+b > c and b + c > a and c + a > b:
+#        print("True")
+ #       return True
+ #   else:
+ #       print("False")
+#        return False
+#a=int(input("a="))
+#b=int(input("b= "))
+#c=int(input("c= "))
+#fuction8(a,b,c)
+
+#10 zadanie
+#def fuction10(a,b,c):
+#    if c == "+":
+#        return a+b
+#    elif c == "-":
+#        return a-b
+#    elif c == "*":
+#        return a*b
+#    elif c == "/":
+#        return a/b
+#    else:
+#        print("ERROR!")
+#a=int(input("a="))
+#b=int(input("b= "))
+#c=int(input("c= "))
+#fuction10(a,b,c)
+
+#11 zadanie
+#def fuction11(tag,text):
+#    print(f"<{tag}>{text}</{tag}>")
+
+#tag=str(input("tag= "))
+#text=str(input("text= "))
+#fuction11(tag,text)
+
+#12 zadanie
